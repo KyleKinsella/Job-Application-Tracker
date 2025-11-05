8b60bd8c1ef73c529dfba980ae304174d18b083d
KyleKinsella <113980354+KyleKinsella@users.noreply.github.com>
2025-11-05 22:53:22 +0000

    Today (5/11/25), I have now implemented light mode & dark mode.
    I have refactored my Mode.jsx file a bit, and I have also refactored my Welcome.jsx file.
    
    I have made a new file called "style.css" this is used to set the color of the background and the color of the text depending on if its in light mode or dark mode.

--- Mode.jsx
+++ Mode.jsx
@@ -1,20 +1,10 @@
-import { useState } from 'react'
 import './Mode.css'
 
-function Mode() {
-    const [darkMode, setDarkMode] = useState(false);
-
+function Mode({darkMode, setDarkMode}) {
     return (
-        <div className={darkMode ? "dark-mode" : "light-mode"}>
-            {/* <h1>Mode</h1> */}
-            {/* <p>Here you will be able to toggle between <strong>light</strong> and <strong>dark</strong> mode.</p> */}
-
-            <button className={darkMode ? "light-mode" : "dark-mode"} 
-                onClick={() => setDarkMode(prev => !prev)}
-            >
-                {darkMode ? "Light Mode☀️": "Dark Mode🌙"}
-            </button>
-        </div>
+        <button className={darkMode ? "light-mode" : "dark-mode"} onClick={() => setDarkMode(prev => !prev)}>
+            {darkMode ? "☀️" : "🌙"}
+        </button>
     )
 }
 
--- Welcome.jsx
+++ Welcome.jsx
@@ -2,22 +2,31 @@ import './welcome.css'
 import Nav from './Nav.jsx'
 import Mode from './Mode.jsx'
 import './Mode.css'
+import { useState, useEffect } from 'react'
+import './style.css'
 
 function Welcome() {
-    return (
-        <header>
-            <h1>Job Application Tracker</h1>
-            <p>Welcome! This is a piece of software that you can use, to keep
-                track of all the jobs that you have applied to!    
-                <br /><br />
-                Let's get going, shall we?
-            </p>
+    const [darkMode, setDarkMode] = useState(false);
+    
+    useEffect(() => {
+        document.body.className = darkMode ? "dark" : "light";
+    }, [darkMode])
 
-            <hr />
-            <Nav></Nav>
-            <hr />
-            <Mode></Mode>
-        </header>
+    return (
+        <>
+        <Mode darkMode={darkMode} setDarkMode={setDarkMode} ></Mode>
+
+        <h1>Job Application Tracker</h1>
+        <p>Welcome! This is a piece of software that you can use, to keep
+            track of all the jobs that you have applied to!    
+            <br /><br />
+            Let's get going, shall we?
+        </p>
+
+        <hr />
+        <Nav></Nav>
+        <hr />
+        </>
     )
 }
 
